d262869a9d19cceaaa0543a8eddb28bf7dc095fe
news75 <news75@libero.it>
2016-09-21 14:40:39 +0200

    .

--- src/com/company/kataTest.java
+++ src/com/company/kataTest.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by giuseppe on 21/09/2016.
  */
 public class kataTest {
+
+    @Test
+    public void prova()
+    {
+        
+    }
 
 }
